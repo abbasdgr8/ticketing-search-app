e63932015c8bce2e6a47911b05abd1e78d0a5716
Abbas Attarwala <abbas.attarwala@open.edu.au>
2020-09-07 00:35:22 +1000

    TDD Cycle 2 - Step 1 - Tests with compilation errors

--- src/test/kotlin/com/examples/abbasdgr8/InputDataDeserializerTests.kt
+++ src/test/kotlin/com/examples/abbasdgr8/InputDataDeserializerTests.kt
@@ -7,20 +7,21 @@ import org.amshove.kluent.shouldNotBeNull
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 import java.io.File
+import java.util.*
 
 class InputDataDeserializerTests: Spek({
 
-    Feature("Must be able to read JSON and deserialize tickets.json") {
+    Feature("Must be able to read and deserialize tickets.json") {
 
         val ticketsFile = File("src/test/resources/json/tickets.json")
-        val d10r = InputDataDeserializer()
+        val deserializer = InputDataDeserializer()
 
         Scenario("Unmarshall tickets.json input data") {
 
             lateinit var tickets: List<Ticket>
 
             When("deserializer invoked") {
-                tickets = d10r.readTickets(ticketsFile)
+                tickets = deserializer.readTickets(ticketsFile)
             }
 
             Then("JSON objects get deserialized successfully") {
@@ -28,9 +29,28 @@ class InputDataDeserializerTests: Spek({
                 tickets shouldBeInstanceOf List::class
             }
 
-            Then("Returns List of Ticket model objects and is not empty") {
+            Then("Returns List of Ticket model objects, and is not empty") {
                 tickets.shouldNotBeEmpty()
                 tickets[0] shouldBeInstanceOf Ticket::class
+            }
+
+            Then("All properties of Ticket object should have expected types") {
+                tickets[0]._id shouldBeInstanceOf String::class
+                tickets[0].url shouldBeInstanceOf String::class
+                tickets[0].external_id shouldBeInstanceOf String::class
+                tickets[0].created_at shouldBeInstanceOf Date::class
+                tickets[0].type shouldBeInstanceOf String::class
+                tickets[0].subject shouldBeInstanceOf String::class
+                tickets[0].description shouldBeInstanceOf String::class
+                tickets[0].priority shouldBeInstanceOf String::class
+                tickets[0].status shouldBeInstanceOf String::class
+                tickets[0].submitter_id shouldBeInstanceOf Integer::class
+                tickets[0].assignee_id shouldBeInstanceOf Integer::class
+                tickets[0].organization_id shouldBeInstanceOf Integer::class
+                tickets[0].tags shouldBeInstanceOf Array<String>::class
+                tickets[0].has_incidents shouldBeInstanceOf Boolean::class
+                tickets[0].due_at shouldBeInstanceOf Date::class
+                tickets[0].via shouldBeInstanceOf String::class
             }
         }
     }

60dcf46b6b31f1a544f1ce0da51e8c592cd8ccea
Abbas Attarwala <abbas.attarwala@open.edu.au>
2020-09-07 00:57:03 +1000

    Red test around values for Ticket model object

--- src/test/kotlin/com/examples/abbasdgr8/InputDataDeserializerTests.kt
+++ src/test/kotlin/com/examples/abbasdgr8/InputDataDeserializerTests.kt
@@ -1,6 +1,7 @@
 package com.examples.abbasdgr8
 
 import com.examples.abbasdgr8.model.Ticket
+import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeInstanceOf
 import org.amshove.kluent.shouldNotBeEmpty
 import org.amshove.kluent.shouldNotBeNull
@@ -51,6 +52,25 @@ class InputDataDeserializerTests: Spek({
                 tickets[0].has_incidents shouldBeInstanceOf Boolean::class
                 tickets[0].due_at shouldBeInstanceOf Date::class
                 tickets[0].via shouldBeInstanceOf String::class
+            }
+
+            Then("All properties of Ticket object should have expected values") {
+                tickets[0]._id shouldBeEqualTo "436bf9b0-1147-4c0a-8439-6f79833bff5b"
+                tickets[0].url shouldBeEqualTo "http://initech.zendesk.com/api/v2/tickets/436bf9b0-1147-4c0a-8439-6f79833bff5b.json"
+                tickets[0].external_id shouldBeEqualTo "9210cdc9-4bee-485f-a078-35396cd74063"
+                tickets[0].created_at.toString() shouldBeEqualTo "2016-04-28T11:19:34 -10:00"
+                tickets[0].type shouldBeEqualTo "incident"
+                tickets[0].subject shouldBeEqualTo "A Catastrophe in Korea (North)"
+                tickets[0].description shouldBeEqualTo "Nostrud ad sit velit cupidatat laboris ipsum nisi amet laboris ex exercitation amet et proident. Ipsum fugiat aute dolore tempor nostrud velit ipsum."
+                tickets[0].priority shouldBeEqualTo "high"
+                tickets[0].status shouldBeEqualTo "pending"
+                tickets[0].submitter_id shouldBeEqualTo 38
+                tickets[0].assignee_id shouldBeEqualTo 24
+                tickets[0].organization_id shouldBeEqualTo 116
+                tickets[0].tags shouldBeEqualTo listOf("Ohio", "Pennsylvania", "American Samoa", "Northern Mariana Islands")
+                tickets[0].has_incidents shouldBeEqualTo false
+                tickets[0].due_at shouldBeEqualTo "2016-07-31T02:37:50 -10:00"
+                tickets[0].via shouldBeEqualTo "web"
             }
         }
     }

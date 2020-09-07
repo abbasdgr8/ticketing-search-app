046c8ebd81b547ebb8da050632343d2de2ef7a8d
Abbas Attarwala <abbas.attarwala@open.edu.au>
2020-09-07 12:51:40 +1000

    TDD Step - Refactor. Removed duplication within InputDataDeserializer class

--- src/main/kotlin/com/examples/abbasdgr8/InputDataDeserializer.kt
+++ src/main/kotlin/com/examples/abbasdgr8/InputDataDeserializer.kt
@@ -4,60 +4,38 @@ import com.examples.abbasdgr8.exception.InputDataDeserializationException
 import com.examples.abbasdgr8.model.Organization
 import com.examples.abbasdgr8.model.Ticket
 import com.examples.abbasdgr8.model.User
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import java.io.File
 import java.lang.Exception
 
+
 class InputDataDeserializer {
 
-    private val gson = GsonBuilder()
-                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss X")
-                        .create()
+    fun readTickets(ticketsJsonFile: File): List<Ticket> = deserializeJson(ticketsJsonFile)
+    fun readUsers(usersJsonFile: File): List<User> = deserializeJson(usersJsonFile)
+    fun readOrganizations(orgsJsonFile: File): List<Organization> = deserializeJson(orgsJsonFile)
 
-    @Throws(InputDataDeserializationException::class)
-    fun readTickets(jsonFile: File): List<Ticket> {
+    companion object {
 
-        val tickets: List<Ticket>
-        val ticketsType = object : TypeToken<ArrayList<Ticket>>() {}.type
+        private val gson: Gson = GsonBuilder()
+                                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss X")
+                                    .create()
 
-        try {
-            tickets = gson.fromJson<List<Ticket>>(jsonFile.readText(), ticketsType)
-        } catch (e: Exception) {
-            throw InputDataDeserializationException(e.message, e.cause)
+        @Throws(InputDataDeserializationException::class)
+        private inline fun <reified T> deserializeJson(jsonFile: File): ArrayList<T> {
+            val objects: ArrayList<T>
+            val type = TypeToken.getParameterized(ArrayList::class.java, T::class.java).type
+
+            try {
+                objects = gson.fromJson<ArrayList<T>>(jsonFile.readText(), type)
+            } catch (e: Exception) {
+                throw InputDataDeserializationException(e.message, e.cause)
+            }
+
+            return objects
         }
 
-        return tickets;
     }
-
-    @Throws(InputDataDeserializationException::class)
-    fun readUsers(jsonFile: File): List<User> {
-
-        val users: List<User>
-        val usersType = object : TypeToken<ArrayList<User>>() {}.type
-
-        try {
-            users = gson.fromJson<List<User>>(jsonFile.readText(), usersType)
-        } catch (e: Exception) {
-            throw InputDataDeserializationException(e.message, e.cause)
-        }
-
-        return users;
-    }
-
-    @Throws(InputDataDeserializationException::class)
-    fun readOrganizations(jsonFile: File): List<Organization> {
-
-        val orgs: List<Organization>
-        val orgsType = object : TypeToken<ArrayList<Organization>>() {}.type
-
-        try {
-            orgs = gson.fromJson<List<Organization>>(jsonFile.readText(), orgsType)
-        } catch (e: Exception) {
-            throw InputDataDeserializationException(e.message, e.cause)
-        }
-
-        return orgs;
-    }
-
 }
